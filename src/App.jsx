7a4815323dac2b9b9f41c1af8b9a94607ab89f8c
viktoriia-yash <yashkina.vic@gmail.com>
2023-04-08 23:05:38 +0200

    I tried to fix github pages deployment and broke netlify' instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./components/header";
 import Home from "./pages/Home/home";
